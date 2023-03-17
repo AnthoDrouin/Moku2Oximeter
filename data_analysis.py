3556e463ad003aefe5ceb29015c6e7039e07e5c4
Anthony Drouin <93488840+AnthoDrouin@users.noreply.github.com>
2023-03-16 20:05:40 -0400

    modify code after testing

--- data_analysis.py
+++ data_analysis.py
@@ -5,122 +5,139 @@ import scipy
 from moku.instruments import Datalogger
 from sigfig import round
 from typing import *
+import time
 
 
 class Oximeter:
-	normal_hearth_rate_by_age = {
-		10: (70, 190),
-		20: (95, 162),
-		30: (93, 157),
-		40: (90, 153),
-		45: (88, 149),
-		50: (85, 145),
-		55: (83, 140),
-		60: (80, 136),
-		65: (78, 132),
-		70: (75, 128),
-	}
-	normal_sp02_by_pourcentage = {
-		67: "DANGER : Cyanosis",
-		80: "LOW OXYGEN LEVELS : Brain is affected",
-		91: "Concerning blood Oxygen Levels : LOW BLOOD OXYGEN LEVELS",
-		95: "Concerning blood Oxygen Levels",
-		100: "normal",
-	}
+	"""
+	This class is used to get data from the oximeter and compute the SpO2 and BPM.
+	"""
 
 	def __init__(
 			self,
-			datalogger: Datalogger,
+			ip: str,
 			patient: Optional[dict] = None,
 			streaming_period: float = 10,
 			filename: Optional[str] = None,
 			show_data: bool = True,
 			**kwargs
 	):
-		self.datalogger = datalogger
+		self.ip = ip
+		self.datalogger = Datalogger(self.ip, force_connect=True)
 		self.patient = patient
 		self.kwargs = kwargs
 		self._set_default_kwargs()
 		self.filename = filename
+		self.streaming_period = streaming_period
 		self.show_data = show_data
 		self.data = {
 			"infrared" : {"ch1": [0], "time": [0]},
-			"red" : None,
+			"red" : {"ch1": [0], "time": [0]},
 			"BPM" : None,
 			"SpO2" : None,
+			"all_BPMs" : None,
 			"health" : [],
+		}
+		self.normal_hearth_rate_by_age = {
+			10: (70, 190),
+			20: (95, 162),
+			30: (93, 157),
+			40: (90, 153),
+			45: (88, 149),
+			50: (85, 145),
+			55: (83, 140),
+			60: (80, 136),
+			65: (78, 132),
+			70: (75, 128),
+		}
+		self.normal_sp02_by_pourcentage = {
+			67: "DANGER : Cyanosis",
+			80: "LOW OXYGEN LEVELS : Brain is affected",
+			91: "Concerning blood Oxygen Levels : LOW BLOOD OXYGEN LEVELS",
+			95: "Concerning blood Oxygen Levels",
+			100: "normal",
 		}
 
 
 	def _set_default_kwargs(self):
 		self.kwargs.setdefault("acquisition_mode", "Precision")
 		self.kwargs.setdefault("sample_rate", 4030)
-		self.kwargs.setdefault("sleep_time", 0)
+		self.kwargs.setdefault("sleep_time", 1)
 		self.kwargs.setdefault("filename", None)
 		self.kwargs.setdefault("plot_data", True)
 		self.kwargs.setdefault("remove_first_data", 0)
 		self.kwargs.setdefault("moving_average_window", 300)
-		self.kwargs.setdefault("param_savgol", (200, 500))
+		self.kwargs.setdefault("param_savgol", (400, 500))
 		self.kwargs.setdefault("batch_size", 1000)
 
 
 	def _set_param_streaming(self):
+		"""
+		Set the parameters of the streaming
+		"""
 		self.datalogger.set_acquisition_mode(mode=self.kwargs["acquisition_mode"])
 		self.datalogger.set_samplerate(self.kwargs["sample_rate"])
 
 	def on_acquisition_begin(self):
+		"""
+		Apply at the beginning of the acquisition
+		"""
 		self._set_param_streaming()
 
 		self.datalogger.generate_waveform(type="DC", channel=1, dc_level=5)
 		self.datalogger.generate_waveform(type="DC", channel=2, dc_level=0)
 
-		# TODO: CONFIRME THAT NO START_STREAMING IS NEEDED
-		#self.datalogger.start_streaming()
-		finger_detected = False
-		while not finger_detected:
-			finger_detected = self.datalogger.get_stream_data()["ch1"][-1] < 4.8
-			print("No finger detected, please put your finger on the sensor", end="\r")
-
-		#self.datalogger.stop_streaming()
-
 
 	def acquisition(self):
+		"""
+		Acquisition of the data
+		"""
 		self.on_acquisition_begin()
-		print(f"Finger detected, starting sleep time of {self.kwargs['sleep_time']}s")
-		time.sleep(self.kwargs["sleep_time"])
 
-		self.datalogger.start_streaming(duration=self.streaming_period)
+		self.datalogger.start_streaming(duration=self.streaming_period*2)
+
 		print(f"Streaming started for red light {self.streaming_period}s")
 
 		while self.data["red"]["time"][-1] < self.streaming_period:
 			new_data = self.datalogger.get_stream_data()
 			self.data["red"]["ch1"] += new_data["ch1"]
 			self.data["red"]["time"] += new_data["time"]
-			print(f"Remaining time {round(duration - data['time'][-1], 3)} seconds", end="\r")
+			print(f"Remaining time {round(self.streaming_period - self.data['red']['time'][-1], 3)} seconds")
 
 		self.datalogger.stop_streaming()
-		time.sleep(1)
-		self.datalogger.start_streaming(duration=self.streaming_period)
+
+		self.datalogger.relinquish_ownership()
+		time.sleep(3)
+		self.datalogger = Datalogger(self.ip, force_connect=True)
+		self._set_param_streaming()
+
 
 		self.datalogger.generate_waveform(type="DC", channel=1, dc_level=0)
 		self.datalogger.generate_waveform(type="DC", channel=2, dc_level=5)
 
 		self.datalogger.start_streaming(duration=self.streaming_period)
-		time.sleep(self.kwargs["sleep_time"])
+
 
 		print(f"Streaming started for infrared light {self.streaming_period}s")
 		while self.data["infrared"]["time"][-1] < self.streaming_period:
 			new_data = self.datalogger.get_stream_data()
 			self.data["infrared"]["ch1"] += new_data["ch1"]
 			self.data["infrared"]["time"] += new_data["time"]
-			print(f"Remaining time {round(duration - data['time'][-1], 3)} seconds", end="\r")
+			print(f"Remaining time {round(self.streaming_period - self.data['infrared']['time'][-1], 3)} seconds")
 
 		self.datalogger.stop_streaming()
+
+		plt.plot(self.data["infrared"]["time"], self.data["infrared"]["ch1"])
+		plt.plot(self.data["red"]["time"], self.data["red"]["ch1"])
+		plt.show()
 
 		self.on_acquisition_end()
 
 
 	def on_acquisition_end(self):
+		"""
+		Apply at the end of the acquisition
+		"""
 		self.data["infrared"]["ch1"] = np.array(self.data["infrared"]["ch1"][1:])
 		self.data["infrared"]["time"] = np.array(self.data["infrared"]["time"][1:])
 		self.data["red"]["ch1"] = np.array(self.data["red"]["ch1"][1:])
@@ -131,59 +148,54 @@ class Oximeter:
 
 		bpm_red = Oximeter.compute_bpm(
 			self.data["red"],
-			remove_first_data=kwargs.get("remove_first_data", 0),
-			param_savgol=kwargs.get("param_savgol", (200, 500)),
-			moving_average_window=kwargs.get("moving_average_window", 300),
+			remove_first_data=self.kwargs.get("remove_first_data", 0),
+			param_savgol=self.kwargs.get("param_savgol", (200, 500)),
+			moving_average_window=self.kwargs.get("moving_average_window", 300),
 		)
 		bpm_infrared = Oximeter.compute_bpm(
 			self.data["infrared"],
-			remove_first_data=kwargs.get("remove_first_data", 0),
-			param_savgol=kwargs.get("param_savgol", (200, 500)),
-			moving_average_window=kwargs.get("moving_average_window", 300),
+			remove_first_data=self.kwargs.get("remove_first_data", 0),
+			param_savgol=self.kwargs.get("param_savgol", (200, 500)),
+			moving_average_window=self.kwargs.get("moving_average_window", 300),
 		)
-		self.data["BPM"] = (bpm_red + bpm_infrared) / 2
+		self.data["BPM"] = (bpm_red[0] + bpm_infrared[0])/ 2
 
-		self.data["sp02"] = Oximeter.compute_sp02(
-			self.data,
-			remove_first_data=kwargs.get("remove_first_data", 0),
-			param_savgol=kwargs.get("param_savgol", (200, 500)),
-			moving_average_window=kwargs.get("moving_average_window", 300),
-			batch_size=kwargs.get("batch_size", 1000),
+		self.data["all_BPMs"] = [bpm_red[0], bpm_infrared[0]]
+
+		self.data["SpO2"] = Oximeter.compute_spO2(
+			data=self.data,
+			param_savgol=(int(-27.5174*self.data["BPM"] + 4254.2561),None),
+			batch_size=int(149.98488*self.data["BPM"] - 8183.47294)
 		)
-
+		# param_savgol and batch_size were determine empirically
 		self.check_health()
 
 		if self.filename is not None:
-			np.save("dataOximeter/" + filename + ".npy", self.data)
+			np.save(self.filename + ".npy", self.data)
 
-		if self.show_data():
-			Oximeter.plot_data(
-				time=self.data["red"]["time"],
-				tension=self.data["red"]["ch1"],
-				type_graph="final",
-			)
+
 
 	def check_health(self) -> None:
 		assert isinstance(self.patient, dict), "the patient informations must be a dict"
 		assert self.patient.get("age") is not None, "The age of the patient must be provided to determine it's health"
 
-		for age in normal_hearth_rate_by_age.keys():
+		for age in self.normal_hearth_rate_by_age.keys():
 			if self.patient["age"] < age:
-				if self.data["BPM"] < normal_hearth_rate_by_age[age][0]:
-					self.data["health"].append(f"bpm under the average {normal_hearth_rate_by_age[age][0]} - {normal_hearth_rate_by_age[age][1]}")
-				elif self.data["BPM"] > normal_hearth_rate_by_age[age][1]:
-					self.data["health"].append(f"bpm above the average {normal_hearth_rate_by_age[age][0]} - {normal_hearth_rate_by_age[age][1]}")
+				if self.data["BPM"] < self.normal_hearth_rate_by_age[age][0]:
+					self.data["health"].append(f"bpm under the average {self.normal_hearth_rate_by_age[age][0]} - {self.normal_hearth_rate_by_age[age][1]}")
+				elif self.data["BPM"] > self.normal_hearth_rate_by_age[age][1]:
+					self.data["health"].append(f"bpm above the average {self.normal_hearth_rate_by_age[age][0]} - {self.normal_hearth_rate_by_age[age][1]}")
 				else:
-					self.data["health"].append(f"Your BPM is as expected for you age. Your BPM is in the following range {normal_hearth_rate_by_age[age][0]} - {normal_hearth_rate_by_age[age][1]}")
+					self.data["health"].append(f"Your BPM is as expected for you age. Your BPM is in the following range {self.normal_hearth_rate_by_age[age][0]} - {self.normal_hearth_rate_by_age[age][1]}")
 				break
 			else:
 				continue
-		if normal_hearth_rate_by_age[normal_hearth_rate_by_age.keys()[-1]] < self.patient["age"]:
+		if 70 < self.patient["age"]:
 			self.data["health"].append(f"No data is available for your age. Sorry :(")
 
-		for normal_concentration in normal_sp02_by_pourcentage.keys():
-			if self.data["SpO2"] < normal_concentration:
-				self.data["health"].append(f"{normal_sp02_by_pourcentage[normal_concentration]}")
+		for normal_concentration in self.normal_sp02_by_pourcentage.keys():
+			if self.data["SpO2"][0] < normal_concentration:
+				self.data["health"].append(f"{self.normal_sp02_by_pourcentage[normal_concentration]}")
 				break
 
 
@@ -216,16 +228,23 @@ class Oximeter:
 		else:
 			return {"time": np.array(time), "ch1": np.array(tension)}
 
-
+	@staticmethod
 	def compute_spO2(
-			self,
 			data: Optional[dict] = None,
 			path: Optional[str] = None,
 			remove_first_data: Optional[int] = None,
-			param_savgol: Tuple[int] = (200, 500),
-			moving_average_window: int = 300,
-			batch_size: int = 1000,
-	):
+			param_savgol: Tuple[int] = (3025, 500),
+			batch_size: int = 2000,
+	) -> Tuple[float, float]:
+		"""
+		Compute the SpO2
+		:param data: data to compute the SpO2
+		:param path: path to the data
+		:param remove_first_data: remove the first data
+		:param param_savgol: parameters for the savgol filter
+		:param batch_size: size of the batch
+		:return: SpO2 and the confidence
+		"""
 		time_red, tension_red = Oximeter.prepare_data(data["red"], path)
 		time_ir, tension_ir = Oximeter.prepare_data(data["infrared"], path)
 		if remove_first_data is not None:
@@ -235,7 +254,20 @@ class Oximeter:
 		time_red_f1, tension_red_f1 = time_red, Oximeter.apply_savgol_filter(tension_red, param_savgol[0])
 		time_ir_f1, tension_ir_f1 = time_ir, Oximeter.apply_savgol_filter(tension_ir, param_savgol[0])
 
-		assert len(time_red_f1) == len(tension_red_f1) == len(time_ir_f1) == len(tension_ir_f1), "Data length mismatch"
+		time_red_f1 = time_red_f1[1::]
+		tension_red_f1 = tension_red_f1[1::]
+
+		len_data_red = len(time_red_f1)
+		len_data_ir = len(time_ir_f1)
+		if len_data_ir > len_data_red:
+			time_ir_f1 = time_ir_f1[:len_data_red]
+			tension_ir_f1 = tension_ir_f1[:len_data_red]
+		elif len_data_red > len_data_ir:
+			time_red_f1 = time_red_f1[:len_data_ir]
+			tension_red_f1 = tension_red_f1[:len_data_ir]
+
+
+		len_data = len(time_red_f1)
 
 		# seperate data in batch_size
 		batches_red = [tension_red_f1[i:i + batch_size] for i in range(0, len_data, batch_size)]
@@ -247,14 +279,13 @@ class Oximeter:
 			batch_red, batch_ir = batch
 			ac_red_div_dc_red = (np.max(batch_red) - np.min(batch_red))/np.min(batch_red)
 			ac_ir_div_dc_ir = (np.max(batch_ir) - np.min(batch_ir))/np.min(batch_ir)
-			#spo2_predicted.append(110 - 25 * ac_red_div_dc_red/ac_ir_div_dc_ir)
-			spo2_predicted.append(ac_red_div_dc_red/ac_ir_div_dc_ir)
+			spo2_predicted.append(110 - 25 * ac_red_div_dc_red/ac_ir_div_dc_ir)
+			#spo2_predicted.append(110 - 25 * np.mean(batch_red)/np.mean(batch_ir))
+			#spo2_predicted.append(110 - 25 * 630/940 * np.log10(np.mean(batch_red)) / np.log10(np.mean(batch_ir)))
+			#R = ac_red_div_dc_red/ac_ir_div_dc_ir
+			#spo2_predicted.append((1.5958422 * R**2) -34.6596622*R + 112.6899759)
 
 		return np.mean(spo2_predicted), np.std(spo2_predicted)
-
-	@staticmethod
-	def _return_max_min(element):
-		return np.max(element), np.min(element)
 
 	@staticmethod
 	def compute_bpm(
@@ -296,6 +327,12 @@ class Oximeter:
 
 	@staticmethod
 	def prepare_data(data: Optional[dict], path: Optional[str] = None) -> Tuple[np.ndarray, np.ndarray]:
+		"""
+		Prepare the data
+		:param data: data to prepare
+		:param path: path to the data
+		:return: time and tension data
+		"""
 		if path is None and data is None:
 			raise ValueError("You must specify a path or give a dataset")
 		if path is not None:
@@ -329,6 +366,9 @@ class Oximeter:
 
 	@staticmethod
 	def moving_average(data_to_average: np.ndarray, window:int = 300) -> np.ndarray:
+		"""
+		Apply a moving average to the data
+		"""
 		return np.convolve(data_to_average, np.ones(window), 'valid') / window
 
 	@staticmethod
@@ -371,7 +411,8 @@ class Oximeter:
 				plt.scatter(time[kwargs.get("index_minimum_time")], tension[kwargs.get("index_minimum_time")])
 			plt.xlabel("Temps [s]", fontsize=22)
 			plt.ylabel("Tension [V]", fontsize=22)
-			plt.title(f"Fréquence cardiaque {kwargs.get('bpm'):.2f}", fontsize=22)
+			if kwargs.get("title") is not None:
+				plt.title(kwargs.get("title"), fontsize=22)
 		elif type_graph == "raw":
 			raise NotImplementedError
 		elif type_graph == "detection":
@@ -382,20 +423,15 @@ class Oximeter:
 			plt.show()
 
 
-class Holter(Oximeter):
-
-	def __init__(self):
-		pass
-
-if __name__ == '__main__':
-	data_dict = Oximeter.extract_data("data_antoine.npy", to_dict=True)
+#if __name__ == '__main__':
+	#data_dict = Oximeter.extract_data("data_antoine.npy", to_dict=True)
 	#data_dict = Oximeter.extract_data("data_antho_real_good.npy", to_dict=True)
-	bpm, extra = Oximeter.compute_bpm(data=data_dict, remove_first_data=100)
-	Oximeter.plot_data(
-		extra["time"],
-		extra["tension_f1"],
-		type_graph="final",
-		index_minimum_time=extra["index_minimum_time"],
-		index_minimum=extra["index_minimum"],
-		bpm=bpm
-	)
+	# bpm, extra = Oximeter.compute_bpm(data=data_dict, remove_first_data=100)
+	# Oximeter.plot_data(
+	# 	extra["time"],
+	# 	extra["tension_f1"],
+	# 	type_graph="final",
+	# 	index_minimum_time=extra["index_minimum_time"],
+	# 	index_minimum=extra["index_minimum"],
+	# 	bpm=bpm
+	# )
--- main.py
+++ main.py
@@ -1,1 +1,49 @@
-pass
+from data_analysis import Oximeter
+from moku.instruments import Datalogger
+import moku
+import sys
+import numpy as np
+from tqdm import tqdm
+
+# datalogger = Datalogger("10.248.117.55", force_connect=True)
+patient = {
+	"name": "Émile J Knystautas",
+	"age": 21,
+}
+# Oximeter(
+# 	ip = "10.248.117.55",
+# 	datalogger=None,
+# 	patient=patient,
+# 	streaming_period=9,
+# 	filename="data_2023_03_16",
+# 	show_data=True,
+# 	remove_first_data=100,
+# ).acquisition()
+
+#print(Oximeter.data["SpO2"], Oximeter.data["BPM"], Oximeter.data["health"])
+
+data = np.load("data_antho_real_good.npy", allow_pickle=True).item()
+bpm, extra = Oximeter.compute_bpm(data=data, remove_first_data=10)
+
+# sp02 = []
+# batch = []
+#
+# for i in tqdm(range(5, 8000, 20)):
+# 	sp02.append(Oximeter.compute_spO2(data=data, remove_first_data=10, param_savgol=(i, 500), batch_size=2005)[0])
+# 	batch.append(i)
+
+#print(max(sp02), batch[sp02.index(max(sp02))])
+
+#print(data["BPM"], data["all_BPMs"][-1] - data["BPM"])
+
+
+#sp02, err = Oximeter.compute_spO2(data=data, remove_first_data=100, param_savgol=(int(-27.5174*data["BPM"] + 4254.2561),None), batch_size=int(149.98488*data["BPM"] - 8183.47294))
+#print(sp02, err)
+Oximeter.plot_data(
+	extra["time"],
+	extra["tension_f1"],
+	type_graph="final",
+	#index_minimum_time=extra["index_minimum_time"],
+	#index_minimum=extra["index_minimum"],
+	#bpm=data["BPM"],
+)
